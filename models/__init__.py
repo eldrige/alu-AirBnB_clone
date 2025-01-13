cbd9db7b494081b32ca54cd1fd5ad95245fae372
Eldrige <peldrige8@gmail.com>
2025-01-13 12:24:22 +0200

    fix: base_model.py

---
+++ models/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module serves as the initialization file for the models package.
+"""
+from models.base_model import BaseModel
+
+
+classes = {
+    "BaseModel": BaseModel,
+}
---
+++ models/base_model.py
@@ -0,0 +1,33 @@
+import uuid
+import datetime
+
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.datetime.strptime(
+                        value, '%Y-%m-%dT%H:%M:%S.%f')
+                elif key != '__class__':
+                    setattr(self, key, value)
+
+    def save(self):
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        return f"{self.__class__.__name__}({self.id})"
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = self.__class__.__name__
+        for key, value in self.__dict__.items():
+            if key == 'created_at' or key == 'updated_at':
+                value = value.isoformat()
+            obj_dict[key] = value
+
+        return obj_dict
