e2ec5971271f7f896cec8ad98510e3467f2f1b5b
Eldrige <peldrige8@gmail.com>
2025-01-15 12:00:40 +0200

    fix: hard coded base-model

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,30 +1,32 @@
 import json
-from models.base_model import BaseModel
+# from models.base_model import BaseModel
 
 
 class FileStorage:
-    __file_path = "file.json"
-    __objects = {}
+
+    def __init__(self):
+        self.__file_path = "file.json"
+        self.__objects = {}
 
     def all(self):
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         key = obj.__class__.__name__ + '.' + obj.id
-        FileStorage.__objects[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
         obj_dict = {}
-        for key, value in FileStorage.__objects.items():
+        for key, value in self.__objects.items():
             obj_dict[key] = value.to_dict()
-        with open(FileStorage.__file_path, 'w') as file:
+        with open(self.__file_path, 'w') as file:
             json.dump(obj_dict, file)
 
     def reload(self):
         try:
-            with open(FileStorage.__file_path, 'r') as file:
+            with open(self.__file_path, 'r') as file:
                 obj_dict = json.load(file)
-
+                from models.base_model import BaseModel
                 for key, value in obj_dict.items():
                     self.__objects[key] = eval(
                         f"{value['__class__']}(**{value})")
