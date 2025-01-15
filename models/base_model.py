679e88e9a3e222b76f13859afa4d7a8067717ada
Eldrige <peldrige8@gmail.com>
2025-01-15 11:53:49 +0200

    fix: storage

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,6 @@
 import uuid
 import datetime
+from models import storage
 
 
 class BaseModel:
@@ -8,6 +9,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
+            storage.new(self)
         else:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
@@ -18,6 +20,7 @@ class BaseModel:
 
     def save(self):
         self.updated_at = datetime.datetime.now()
+        storage.save()
 
     def __str__(self):
         return f"{self.__class__.__name__}({self.id})"
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,32 +1,33 @@
 import json
+from models.base_model import BaseModel
 
 
 class FileStorage:
-    def __init__(self):
-        self.__file_path = "file.json"
-        self.__objects = {}
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         key = obj.__class__.__name__ + '.' + obj.id
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
         obj_dict = {}
-        for key, value in self.__objects.items():
+        for key, value in FileStorage.__objects.items():
             obj_dict[key] = value.to_dict()
-        with open(self.__file_path, 'w') as file:
+        with open(FileStorage.__file_path, 'w') as file:
             json.dump(obj_dict, file)
 
     def reload(self):
-        if self.__file_path:
-            try:
-                with open(self.__file_path, 'r') as file:
-                    obj_dict = json.load(file)
-                    for key, value in obj_dict.items():
-                        class_name, obj_id = key.split('.')
-                        self.__objects[key] = eval(class_name)(**value)
-            except FileNotFoundError:
-                pass
+        try:
+            with open(FileStorage.__file_path, 'r') as file:
+                obj_dict = json.load(file)
+
+                for key, value in obj_dict.items():
+                    self.__objects[key] = eval(
+                        f"{value['__class__']}(**{value})")
+
+        except FileNotFoundError:
+            pass
