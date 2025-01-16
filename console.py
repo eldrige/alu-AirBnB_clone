e4386b4c00838ae5178101bfabe4a790b20cf345
Eldrige <peldrige8@gmail.com>
2025-01-16 14:03:41 +0200

    docs: console.py

--- console.py
+++ console.py
@@ -1,24 +1,29 @@
-#!/usr/bin/env python3
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    def do_quit(self, line):
+    """Command interpreter for HBNB project."""
+
+    intro = 'Welcome to the HBNB command interpreter. Type help or ? to list commands.'
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit the command interpreter."""
+        print('Exiting HBNB command interpreter.')
         return True
 
-    def do_EOF(self, line):
+    def do_EOF(self, arg):
+        """Exit the command interpreter on EOF signal."""
+        print('Exiting HBNB command interpreter.')
         return True
-
-    def do_help(self, line):
-        print("Documented commands (type help <topic>):")
-        print("========================================")
-        print("EOF  help  quit")
 
     def emptyline(self):
-        """
-        An empty line + ENTER shouldn't execute anything
-        """
+        """Override empty line behavior to do nothing."""
         pass
+
+    def do_help(self, arg):
+        """Display help for commands."""
+        super().do_help(arg)
 
 
 if __name__ == '__main__':
